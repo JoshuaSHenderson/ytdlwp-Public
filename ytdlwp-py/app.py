184ac8206455b24f545226016d1034dfb79d132b
Josh Henderson <jhenderson@vpocloud.com>
2024-03-15 16:33:53 -0400

    cleaning

--- ytdlwp-py/app.py
+++ ytdlwp-py/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify as flask_jsonify
 import subprocess
 import os
+# import jsonify
 
 app = Flask(__name__)
 
@@ -14,15 +15,19 @@ def run_command():
     showName = request.form['showName']
     # command =  f'"C:\\Program Files (x86)\\ytdl\\youtube-dl.exe" -url {link} -o "C:\\ToRename\\TVTorename\\{showName}\\%(title)s.%(ext)s"'
     # for Testing
-    command =  f'echo "-url {link} -o C:\\ToRename\\TVTorename\\{showName}\\%(title)s.%(ext)s"'
+    command =  f'timeout 3 && echo "-url {link} -o C:\\ToRename\\TVTorename\\{showName}\\%(title)s.%(ext)s"'
+
+    successMessg = f"{showName}  was downloaded to ToRename\{showName}\."
 
     try:
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         output = result.stdout
         result2 = os.popen(command)
-        return f"Success: <br> {command}<pre>{output}</pre>"
+        return flask_jsonify({'success': True, 'output': successMessg})
+        # return f"Success: <br> {command}<pre>{output}</pre>"
     except Exception as e:
-        return f"Failed to execute command: {e}"
+        return flask_jsonify({'success': False, 'error': str(e)})
+        # return f"Failed to execute command: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
